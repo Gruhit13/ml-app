2258f016bcef3abf86545101a50d36135e4a3e43
Gruhit13 <gruhitspatel15@gmail.com>
2024-06-02 08:39:50 -0400

    Performed XGBoost Classifier

--- app.py
+++ app.py
@@ -1,22 +1,59 @@
-from flask import Flask, request, jsonify
-import pickle
+import streamlit as st
 import numpy as np
+import joblib
+import pandas as pd
 
-app = Flask(__name__)
+# Category to flower type
+category_to_flower = {
+    0: 'setosa',
+    1: 'versicolor',
+    2: 'virginica'
+}
 
-with open('model.pkl', 'rb') as f:
-	model = pickle.load(f)
+# Set title for the webapp
+st.title("Iris Flower Classifier")
 
-@app.route('/')
-def hello_world():
-	return 'Hello, Docker!'
+# Description
+st.markdown('A classifier model to identify the type of iris flower based on the paramters')
 
+st.header("Plant features")
+cols = st.columns(2)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-	data = request.get_json(force=True)
-	prediction = model.predict(np.array(data['input']).reshape(1, -1))
-	return jsonify({'prediction': int(prediction)})
+with cols[0]:
+    st.text("Sepal Features")
+    sepal_length = st.slider("Sepal Length (cm)", 1.0, 8.0, 0.5)
+    sepal_width = st.slider("Sepal Width (cm)", 2.0, 4.4, 0.5)
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80)
+with cols[1]:
+    st.text("Petal Features")
+    petal_length = st.slider("Petal Length (cm)", 1.0, 7.0, 0.5)
+    petal_width = st.slider("Petal Width (cm)", 0.1, 2.5, 0.5)
+
+# Leave an exmpty space
+st.text('')
+if st.button('Predict Flower'):
+    # Load the model
+    clf = joblib.load('./model.sav')
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = clf.predict_proba(features)
+    st.write(prediction)
+
+    pred_label = np.argmax(prediction, axis=-1)
+    st.markdown(f':green[The flower is {category_to_flower[pred_label[0]].capitalize()}]')
+
+    prediction_df = pd.DataFrame(
+        {
+            'classes': list(category_to_flower.values()),
+            'probability': prediction[0]
+        }
+    )
+
+    st.bar_chart(
+        data = prediction_df,
+        x = 'classes',
+        y = 'probability',
+        color = 'classes'
+    )
+
+    max_prob = max(prediction[0])
+    st.markdown(f'#### Graph suggest that the classifier is :blue[{max_prob*100:.2f}%] sure of its decision.')
--- train_model.py
+++ train_model.py
@@ -1,12 +1,42 @@
+from xgboost import XGBClassifier
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
-import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
 
 iris = load_iris()
 X, y = iris.data, iris.target
+labels = list(iris.target_names)
 
-clf = DecisionTreeClassifier()
-clf.fit(X, y)
+X_train, X_test, y_train, y_test = train_test_split(
+	X,
+	y,
+	test_size = 0.1
+)
 
-with open('model.pkl', 'wb') as f:
-	pickle.dump(clf, f)
+# Train the model
+clf = XGBClassifier(objective='multi:softprob')
+clf.fit(X_train, y_train)
+
+# get prediction from the model
+y_pred = clf.predict(X_test)
+print("Y_pred shape: ", y_pred.shape)
+
+clf_report = classification_report(y_test, y_pred, target_names=labels)
+print(clf_report)
+
+# Write the classification report to a file
+with open('clf_report.txt', 'w') as clf_rprt_file:
+	clf_rprt_file.write(clf_report)
+
+conf_mat = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=labels, yticklabels=labels)
+plt.xlabel("Predicted", {"size":20})
+plt.ylabel("Actual", {"size": 20})
+plt.savefig("confusion_matrix.png")
+
+# Saving the model as joblib to parallelize the process
+with open('model.sav', 'wb') as f:
+	joblib.dump(clf, f)
